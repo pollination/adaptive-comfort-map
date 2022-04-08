55990337c3da47ac03937ea6b2a205e472278550
Chris Mackey <chris@ladybug.tools>
2022-04-08 16:38:59 -0400

    fix(viz): Set up recipe to produce VTK visualizations

--- pollination/adaptive_comfort_map/entry.py
+++ pollination/adaptive_comfort_map/entry.py
@@ -600,6 +600,10 @@ class AdaptiveComfortMapEntryPoint(DAG):
     ) -> List[Dict]:
         return [
             {
+                'from': MapResultInfo()._outputs.viz_config_file,
+                'to': 'metrics/config.json'
+            },
+            {
                 'from': MapResultInfo()._outputs.temperature_info,
                 'to': 'results/temperature/results_info.json'
             },
